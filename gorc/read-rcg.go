d7b71a249788606259b9bc173aec13ede4fa05b6
Mark Harrison <marhar@gmail.com>
2022-05-08 22:02:13 -0700

    reading message headers.  next, get body.

--- gorc/read-rcg.go
+++ gorc/read-rcg.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/emersion/go-imap"
 	"github.com/emersion/go-imap/client"
 	"log"
 	"os"
@@ -23,6 +25,40 @@ func check(e error) {
 	}
 }
 
+func showRCG(c *client.Client) {
+	mbox, err := c.Select("[Gmail]/rcgroups", false)
+	check(err)
+	log.Println("Flags for [Gmail]/rcgroups:", mbox.Flags)
+	seqset := new(imap.SeqSet)
+	seqset.AddRange(1, 99999)
+
+	messages := make(chan *imap.Message, 10)
+	done := make(chan error, 1)
+	go func() {
+		done <- c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
+	}()
+	for msg := range messages {
+		fmt.Println("* " + msg.Envelope.Subject)
+		fmt.Println(msg.Body)
+	}
+	err = <-done
+	check(err)
+}
+func listMailboxes(c *client.Client) {
+	mailboxes := make(chan *imap.MailboxInfo, 10)
+	done := make(chan error, 1)
+	go func() {
+		done <- c.List("", "*", mailboxes)
+	}()
+
+	fmt.Println("Mailboxes:")
+	for m := range mailboxes {
+		fmt.Println("* " + m.Name)
+	}
+	err := <-done
+	check(err)
+}
+
 func main() {
 	user, err := user.Current()
 	check(err)
@@ -38,4 +74,6 @@ func main() {
 	log.Println("Logged in")
 	defer c.Logout()
 
+	//listMailboxes(c)
+	showRCG(c)
 }
